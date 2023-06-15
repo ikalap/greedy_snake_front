fb3d960c1c960de4b71c1b8b1c6aaab12d368831
kalpa <476619022@qq.com>
2023-06-15 23:43:47 +0800

    初始化

---
+++ src/components/Kalpa.vue
@@ -0,0 +1,13 @@
+<script setup>
+const msg = "hello";
+const rawHtml = "<br>world";
+</script>
+
+<template>
+<span>Message: {{ msg }}</span>
+<span v-html="rawHtml"></span>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+// 从一个组件中导入根组件
 import App from './App.vue'
 
 createApp(App).mount('#app')
