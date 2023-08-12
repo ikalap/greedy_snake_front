de2c9695c7d601050b51601747229c67096b10f8
kalpa <476619022@qq.com>
2023-08-13 02:49:35 +0800

    jenkins 配置

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,10 +2,17 @@
 
 echo '现在开始运行vue项目，并且将输出'
 
+echo '执行 run build'
 set -x
-npm run dev &
+npm run build
+set +x
+
+echo '执行 run preview'
+set -x
+npm run preview&
 sleep 1
 echo $! > .pidfile
 set +x
+
 
 echo '现在可以打开http://60.204.169.227:5173/ 接口来访问了'
