6280490dc3cfd8f3b510d7dd15e85dc3eddb877c
kalpa <476619022@qq.com>
2025-02-27 17:24:47 +0800

    部署到云服务器

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -48,6 +48,7 @@ function setDifficulty(newDifficulty) {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 100%;
   height: 100vh;
   background-color: #f0f0f0;
 }
