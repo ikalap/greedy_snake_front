b1a42737c1a984f9618eeea33c7e053be3ca412d
ikalpa <476619022@qq.com>
2025-06-22 15:20:19 +0800

    测试部署

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/greedy_snake_front/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
